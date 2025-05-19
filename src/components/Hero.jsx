3d2a113a822b71c611f5818e4e94e80f6bbfeba5
Mustapha-Hamza <mustapha.hamza7@gmail.com>
2025-05-19 06:16:23 +0100

    Re-named profile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PortfolioPic from "../assets/my_pic.png";
+import PortfolioPic from "../assets/mypic.png";
 import { HERO_CONTENT } from "../constants";
 import { motion } from "framer-motion";
 
